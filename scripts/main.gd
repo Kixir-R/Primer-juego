bc04cba8c0847a3f1b7b0b68c5e48a10d6ab0187
Kixir-R <r.jair456@gmail.com>
2026-03-03 14:47:33 -0600

    Actualización
    Actualización

---
+++ scripts/main.gd
@@ -0,0 +1,46 @@
+extends Node
+
+@export var mob_scene: PackedScene
+var score
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	new_game()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func game_over():
+	$ScoreTimer.stop()
+	$MobTimer.stop()
+	
+func new_game():
+	score = 0
+	$Player.start($StartPosition.position)
+	$StartTimer.start()
+
+func _on_mob_timer_timeout():
+	var mob=mob_scene.instantiate()
+#Elige una ubicación aleatoria sobre "Path2D"
+	var mob_spawn_location=$MobPath/MobSpawnLocation
+	mob_spawn_location.progress_ratio=randf()
+#Configura la posición del enemigo a la aleatorizada
+	mob.position= mob_spawn_location.position
+#Configura la dirección perpendicular del enemigo a la dirección de la ruta
+	var direction= mob_spawn_location.rotation +PI /2
+#Agrega aleatoriedad a la dirección
+	direction+= randf_range(-PI/ 4, PI /4)
+	mob.rotation= direction
+#Elige un vector velocidad para el enemigo
+	var velocity= Vector2(randf_range(150.0, 350.0), 0.0)
+	mob.linear_velocity= velocity.rotated(direction)
+#Hace aparecer al enemigo agregándolo a la escena principal
+	add_child(mob)
+
+func _on_score_timer_timeout():
+	score+= 1
+
+func _on_start_timer_timeout():
+	$MobTimer.start()
+	$ScoreTimer.start()
