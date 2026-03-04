4ec04fd90a29f728b946da5853a23f1e1ae2fcd3
Kixir-R <r.jair456@gmail.com>
2026-03-04 13:52:10 -0600

    actualizacion
    4.03.2026

--- scripts/main.gd
+++ scripts/main.gd
@@ -4,8 +4,7 @@ extends Node
 var score
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	new_game()
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
@@ -14,12 +13,16 @@ func _process(delta: float) -> void:
 func game_over():
 	$ScoreTimer.stop()
 	$MobTimer.stop()
-	
+	$HUD.show_game_over()
+
 func new_game():
 	score = 0
 	$Player.start($StartPosition.position)
 	$StartTimer.start()
-
+	$HUD.update_score(score)
+	$HUD.show_message("¿Listo?")
+	get_tree().call_group("mobs", "queue_free")
+	
 func _on_mob_timer_timeout():
 	var mob=mob_scene.instantiate()
 #Elige una ubicación aleatoria sobre "Path2D"
@@ -40,6 +43,8 @@ func _on_mob_timer_timeout():
 
 func _on_score_timer_timeout():
 	score+= 1
+	$HUD.update_score(score)
+
 
 func _on_start_timer_timeout():
 	$MobTimer.start()
